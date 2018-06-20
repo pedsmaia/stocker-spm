9bf6fcbcf75e09b0d7e6fd7a76fd35b9d6b8ea92
Peds <pedro@lincsmac.co.uk>
2018-06-20 16:19:46 +0100

    navbar links updated

--- link.php
+++ link.php
@@ -7,5 +7,5 @@ $link = mysqli_connect("localhost","lincsmac_stocker","flym4zt3r","lincsmac_stoc
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  } else { echo "Connected";}
+  } // else { echo "Connected";}
 ?>
