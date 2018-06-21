c84bb62372d6d2a8172745be09282f137b29b038
Peds <pedro@lincsmac.co.uk>
2018-06-21 16:55:50 +0100

    login page

--- login.php
+++ login.php
@@ -1,46 +1,112 @@
 <?php
   session_start();
-	if(!empty($_SESSION["user_id"])) {
-		header('location: index.php');
-	}
+  if(!empty($_SESSION["user_id"])) {
+    header('location: index.php');
+  }
   include 'link.php'; 
   if(empty($_SESSION["user_id"])) {
 
-	$message="";
-	if(!empty($_POST["login"])) {
-		$result = mysqli_query($link,"SELECT * FROM users WHERE username='" . $_POST["username"] . "' and password = '". $_POST["password"]."'");
-		$row  = mysqli_fetch_array($result);
-		if(is_array($row)) {
-		$_SESSION["user_id"] = $row['username'];
-		$_SESSION["user_name"] = $row['name'];
-		$_SESSION["position"] = $row['position'];
-		$_SESSION["avatar"] = $row['avatar'];
-		header('location: index.php');
-		} else {
-		$message = "Invalid Username or Password!";
-		}
-	}
-	if(!empty($_POST["logout"])) {
-		$_SESSION["user_id"] = "";
-		session_destroy();
-	}
+  $message="";
+  if(!empty($_POST["login"])) {
+    $result = mysqli_query($link,"SELECT * FROM users WHERE username='" . $_POST["username"] . "' and password = '". $_POST["password"]."'");
+    $row  = mysqli_fetch_array($result);
+    if(is_array($row)) {
+    $_SESSION["user_id"] = $row['username'];
+    $_SESSION["user_name"] = $row['name'];
+    $_SESSION["position"] = $row['position'];
+    $_SESSION["avatar"] = $row['avatar'];
+    header('location: index.php');
+    } else {
+    $message = "Invalid Username or Password!";
+    }
+  }
+  if(!empty($_POST["logout"])) {
+    $_SESSION["user_id"] = "";
+    session_destroy();
+  }
 
  ?>
 
-        <form action="" method="post" name="login">
-          <div class="form-group">
-            <label for="username">Username</label>
-            <input type="text" id="username" class="form-control" placeholder="Enter your username" name="username" required autofocus>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>OurStocker</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="robots" content="all,follow">
+    <!-- Bootstrap CSS-->
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!-- Font Awesome CSS-->
+    <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+    <!-- Fontastic Custom icon font-->
+    <link rel="stylesheet" href="css/fontastic.css">
+    <!-- Google fonts - Poppins -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,700">
+    <!-- theme stylesheet-->
+    <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
+    <!-- Custom stylesheet - for your changes-->
+    <link rel="stylesheet" href="css/custom.css">
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="img/favicon.ico">
+    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+  </head>
+  <body>
+    <div class="page login-page">
+      <div class="container d-flex align-items-center">
+        <div class="form-holder has-shadow">
+          <div class="row">
+            <!-- Logo & Information Panel-->
+            <div class="col-lg-6">
+              <div class="info d-flex align-items-center">
+                <div class="content">
+                  <div class="logo">
+                    <!-- <h4>OUR<strong>STOCKER</strong></h4> -->
+
+                  <div class="brand-text d-none d-lg-inline-block"><span>Our </span><strong>Stocker</strong></div>
+                  <p>Private management system for LincsMaC.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- Form Panel    -->
+            <div class="col-lg-6 bg-white">
+              <div class="form d-flex align-items-center">
+                <div class="content">
+                  <form method="post" class="form-validate">
+                    <div class="form-group">
+                      <input id="login-username" type="text" name="username" required data-msg="Please enter your username" class="input-material">
+                      <label for="login-username" class="label-material">User Name</label>
+                    </div>
+                    <div class="form-group">
+                      <input id="login-password" type="password" name="password" required data-msg="Please enter your password" class="input-material">
+                      <label for="login-password" class="label-material">Password</label>
+                    </div>
+                    <button class="btn btn-lg btn-primary btn-block" name="login" value="Login"  type="submit">Login</button>
+                  </form>
+                </div>
+              </div>
+            </div>
           </div>
-          <div class="form-group">
-            <label for="password" class="sr-only">Password</label>
-          <input type="password" id="password" class="form-control" placeholder="Password" name="password" required>
-          </div>
-          <button class="btn btn-lg btn-primary btn-block" name="login" value="Login"  type="submit">Sign in</button>
-        </form>
+        </div>
+      </div>
+      <div class="copyrights text-center">
+        <p>OUR<strong>STOCKER</strong> by <a href="http://www.peds.me.uk" class="external">Peds</a></p>
       </div>
     </div>
-
-    
+    <!-- JavaScript files-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper.js/umd/popper.min.js"> </script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="vendor/jquery.cookie/jquery.cookie.js"> </script>
+    <script src="vendor/chart.js/Chart.min.js"></script>
+    <script src="vendor/jquery-validation/jquery.validate.min.js"></script>
+    <!-- Main File-->
+    <script src="js/front.js"></script>
+  </body>
+</html>
 
 <? } ?>
---
+++ login2.php
@@ -0,0 +1,46 @@
+<?php
+  session_start();
+	if(!empty($_SESSION["user_id"])) {
+		header('location: index.php');
+	}
+  include 'link.php'; 
+  if(empty($_SESSION["user_id"])) {
+
+	$message="";
+	if(!empty($_POST["login"])) {
+		$result = mysqli_query($link,"SELECT * FROM users WHERE username='" . $_POST["username"] . "' and password = '". $_POST["password"]."'");
+		$row  = mysqli_fetch_array($result);
+		if(is_array($row)) {
+		$_SESSION["user_id"] = $row['username'];
+		$_SESSION["user_name"] = $row['name'];
+		$_SESSION["position"] = $row['position'];
+		$_SESSION["avatar"] = $row['avatar'];
+		header('location: index.php');
+		} else {
+		$message = "Invalid Username or Password!";
+		}
+	}
+	if(!empty($_POST["logout"])) {
+		$_SESSION["user_id"] = "";
+		session_destroy();
+	}
+
+ ?>
+
+        <form action="" method="post" name="login">
+          <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" id="username" class="form-control" placeholder="Enter your username" name="username" required autofocus>
+          </div>
+          <div class="form-group">
+            <label for="password" class="sr-only">Password</label>
+          <input type="password" id="password" class="form-control" placeholder="Password" name="password" required>
+          </div>
+          <button class="btn btn-lg btn-primary btn-block" name="login" value="Login"  type="submit">Sign in</button>
+        </form>
+      </div>
+    </div>
+
+    
+
+<? } ?>
