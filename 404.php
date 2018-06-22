cfe4312cf4353e317d5074e937c4337f125ef1a5
Peds <pedro@lincsmac.co.uk>
2018-06-22 16:32:04 +0100

    404 page and image

---
+++ 404.php
@@ -0,0 +1,40 @@
+<?php
+
+$title = "Page Not Found";
+
+include 'header.php';
+
+?>
+
+<div class="container-fluid">
+<section>
+		<h1 class="display-3 text-center">OOOPS! Page not found...</h1>
+
+<div class="mx-auto mt-3">
+    <div class="card">
+		<div class="card-body">
+
+		<div class="row mx-auto">
+			<div class="col-sm-12 col-md-6 text-right">
+		<img class="img-fluid" src="img/404.jpg">
+			</div>
+
+			<div class="col-sm-12 col-md-4 pt-3">
+			<p>Since this script was coded by Peds, you should never be seeing this page!</p>
+			<p>Something seems to not be working properly, though...</p> 
+			<p>Please try doing whatever it is you were doing again and, IF you end up here again, ignore this message and tell Peds about it.</p>
+			</div>
+		</div><!-- row -->
+	                      
+	    </div><!-- card body -->
+	</div><!-- card -->
+</div><!-- col-lg-6 -->
+
+</section>
+</div><!-- container-fluid -->
+
+<?php
+
+include 'footer.php';
+
+?>
